dc6a73d93ed01eaec72037498462c9bac1b2cff1
xivol <social.aurgtangfovr@gmail.com>
2016-09-30 18:26:14 +0300

    fix

---
+++ Operator/test_point.h
@@ -0,0 +1,17 @@
+//
+// Ћабораторна€ работа є6.  лассы. ќперации
+// test_point.h
+//
+#pragma once
+#include "point.h"
+
+// ќбъ€вление тестирующего класса
+class test_point
+{
+    bool distance_to();
+    bool operator_eq();
+    bool operator_neq();
+public:
+    static bool run();
+};
+
