a44b98cdf5d4c0691b368ada0096842f1bde7ee4
xivol <social.aurgtangfovr@gmail.com>
2016-09-30 16:59:23 +0300

    test first commit

---
+++ UnitTest/point.h
@@ -0,0 +1,8 @@
+#pragma once
+class point
+{
+public:
+    point();
+    ~point();
+};
+
---
+++ UnitTest/polygon.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "polygon.h"
+
+
+polygon::polygon()
+{
+}
+
+
+polygon::~polygon()
+{
+}
