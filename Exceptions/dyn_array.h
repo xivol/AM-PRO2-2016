e0252b79e1ef56fbb3faa87f50cc901276b44742
xivil <loshkarev.i@gmail.com>
2016-10-01 13:42:58 +0300

    fff

--- Exceptions/dyn_array.h
+++ Exceptions/dyn_array.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <exception>
 #include <iostream>
 #include "crtdynmem.h"
 
@@ -20,11 +21,26 @@ public:
 
     void resize(int new_size);
     void append(const datatype &x);
+
+	void insert(int index, const datatype &x);
+	void remove_at(int index);
+	void remove(const datatype &x);
+	bool contains(const datatype &x);
 private:
     datatype *data;
     int size;
     void copy(const datatype *from, datatype* to, int size);
     void file_init(const char *filename);
     void text_init(const char *filename);
+
+public:
+	class array_exception : public std::exception {
+	};
+	class size_exception : public array_exception {
+	};
+	class index_exception : public array_exception {
+	};
+	class file_exception :public array_exception {
+	};
 };
 
