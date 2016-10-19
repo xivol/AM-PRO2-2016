60e300f4e214c16c5b0bf648ca4b378c0b973a9f
xivil <loshkarev.i@gmail.com>
2016-10-19 09:53:23 +0300

    tests added

--- ListIterator/test_list.h
+++ ListIterator/test_list.h
@@ -2,6 +2,10 @@
 class test_list
 {
     bool equals(const list &l1, const list &l2);
+
+	// Тестирующие функции
+	bool copy_list();
+	bool delete_list()
     bool copy_ctor();
     bool assign();
     bool push_back();
