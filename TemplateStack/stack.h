ed0b310950f5e2d8006c5c741437d9a940bf78d7
xivol <loshkarev.i@gmail.com>
2016-11-02 10:11:27 +0300

    template changes

--- TemplateStack/stack.h
+++ TemplateStack/stack.h
@@ -21,35 +21,9 @@ class stack
     node *begin;
 
     // Удаление списка
-    void delete_list()
-    {
-        while (begin != nullptr) {
-            node *t = begin;
-            begin = begin->next;
-            delete t;
-        }
-    }
-
+	void delete_list();
     // Копирование списка
-    void copy_list(const node * l)
-    {
-        node * p = nullptr;
-        while (l != nullptr) {
-            node *t = new node;
-            t->data = l->data;
-            t->next = nullptr;
-
-            if (p != nullptr)
-                p->next = t;
-            else {
-                p = t;
-                begin = t;
-            }
-            p = p->next;
-            l = l->next;
-        }
-    }
-
+	void copy_list(const node * l);
 public:
     stack() : begin(nullptr) {}
 
@@ -60,7 +34,7 @@ public:
 
     stack &operator=(const stack &s)
     {
-        if (!is_empty()) delete_list();
+        delete_list();
         copy_list(s.begin);
     }
 
@@ -74,29 +48,82 @@ public:
         return begin == nullptr;
     }
 
-    void push(const T &x)
-    {
-        node *t = new node;
-        t->data = x;
-        t->next = begin;
-        begin = t;
-    }
+	// Добавление значения в стек
+	void push(const T &x);
 
-    void pop()
-    {
-        if (is_empty()) return;
-        node *t = begin;
-        begin = begin->next;
-        delete t;
-    }
+	// Извлечение с вершины стека
+	void pop();
 
-    T top()
-    {
-        if (is_empty())
-            throw std::out_of_range("Попытка извлечения из пустого стека!");
-        return begin->data;
-    }
-
+	// Просмотр вершины стека
+	T top();
+    
     template <typename P>
     friend std::ostream &operator<<(std::ostream &os, const stack<P> &s);
 };
+
+template <typename T>
+void stack<T>::delete_list()
+{
+	while (begin != nullptr) {
+		node *t = begin;
+		begin = begin->next;
+		delete t;
+	}
+}
+
+template <typename T>
+void stack<T>::copy_list(const node * l)
+{
+	node * p = nullptr;
+	while (l != nullptr) {
+		node *t = new node;
+		t->data = l->data;
+		t->next = nullptr;
+
+		if (p != nullptr)
+			p->next = t;
+		else {
+			p = t;
+			begin = t;
+		}
+		p = p->next;
+		l = l->next;
+	}
+}
+
+template <typename T>
+void stack<T>::push(const T &x)
+{
+	node *t = new node;
+	t->data = x;
+	t->next = begin;
+	begin = t;
+}
+
+template <typename T>
+void stack<T>::pop()
+{
+	if (is_empty()) return;
+	node *t = begin;
+	begin = begin->next;
+	delete t;
+}
+
+template <typename T>
+T stack<T>::top()
+{
+	if (is_empty())
+		throw std::out_of_range("Попытка извлечения из пустого стека!");
+	return begin->data;
+}
+
+template <typename P>
+std::ostream &operator<<(std::ostream &os, const stack<P> &s) 
+{
+	stack<P>::node *t = s.begin;
+	while (t != nullptr) {
+		std::cout << t->data << ' ';
+		t = t->next;
+	}
+	std::cout<<std::endl;
+}
