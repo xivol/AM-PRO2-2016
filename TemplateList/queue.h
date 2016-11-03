ab25cffc988315e6c877074f5143dc168e2702b7
xivol <loshkarev.i@gmail.com>
2016-11-03 12:40:10 +0300

    abstract queue

--- TemplateList/queue.h
+++ TemplateList/queue.h
@@ -1,38 +1,11 @@
 #pragma once
-#include "list.h"
 
 template <typename Datatype>
 class abstract_queue {
+public:
 	virtual ~abstract_queue() = 0 {}
 	virtual bool is_empty() const = 0;
 	virtual void push(const Datatype &x) = 0;
 	virtual void pop() = 0;
 	virtual Datatype top() = 0;
-};
-
-template <typename Datatype>
-class queue : public abstract_queue<Datatype>{
-	list<Datatype> data;
-public:
-	bool is_empty() const
-	{
-		return data.is_empty();
-	}
-
-	void push(const Datatype &x) 
-	{
-		data.push_back(x);
-	}
-
-	Datatype pop() 
-	{
-		Datatype t = data.front();
-		data.pop_front();
-		return t;
-	}
-
-	Datatype top() const
-	{
-		return data.front();
-	}
 };
