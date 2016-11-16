ebcde39255aa380f22614e5b24ccaebe35575f9c
xivol <loshkarev.i@gmail.com>
2016-11-16 13:05:08 +0300

    Comments

--- BinarySearchTree/priority_queue.h
+++ BinarySearchTree/priority_queue.h
@@ -1,48 +1,99 @@
 #pragma once
 #include "bst.h"
 
+// Шаблон элемента для очереди с приоритетом
 template <typename Key, typename Value>
-struct pair
+struct qpair
 {
-    Key key;
-    Value value;
-    bool operator==(const pair &p)
-    {
-        return key == p.key;
-    }
-    bool operator<(const pair &p)
-    {
-        return key < p.key;
-    }
+	Key key;
+	Value value;
+
+	// Конструкторы
+	qpair() {}
+	qpair(const Key&k, const Value &v) : key(k), value(v) {}
+
+	// Сравнение на равенство
+	bool operator==(const qpair &p) const
+	{
+		// равны если совпадают и ключ и значение
+		return key == p.key && value == p.value;
+	}
+	// Сранение на неравенство
+	bool operator<(const qpair &p) const
+	{
+		// сравнение по ключу
+		return key < p.key;
+	}
 };
 
+// Шаблон очереди с приоритетом
 template <typename Priority, typename Value>
-class priority_queue : protected bst<pair<Priority, Value>>
+class priority_queue : protected bst<qpair<Priority, Value>>
 {
-    typedef pair<Priority, Value> pair;
-    typedef bst<pair>::iterator iterator;
+	/* Мы используем завкырое наследование, 
+	   т.к. очередь не является деревом, 
+	   а лишь использует его для хранения элементов.
+	   
+	   При этом, мы не можем использовать дерево как
+	   член класса, т.к. нам нужен доступ 
+	   к его закрытым методам для эффективной работы с ним.
+	   
+	   Не превильная архитектура класса bst! 
+	   Класс должен позволять выполнять операции эффективно, 
+	   без доступа к закрытым полям и методам.
+	*/
+protected:
+	// короткое имя пары
+	typedef qpair<Priority, Value> qpair;
 
-    Priority default;
+	// Приоритет по умолчанию
+	Priority default;
 public:
-    priority_queue() {}
-    priority_queue(const Priority &default): default(default) {}
-	
-	priority_queue(const priority_queue &q): bst<pair>(q) {}
-	priority_queue& operator=(const priority_queue &q);
+	// Конструторы
+	priority_queue() {}
+	priority_queue(const Priority &default) : default(default) {}
 
+	// Копирование
+	priority_queue(const priority_queue &q) : bst<qpair>(q) {}
+	priority_queue& operator=(const priority_queue &q)
+	{
+		bst<qpair>::operator=(q);
+		return *this;
+	}
+
+	// Добавление в очередь
 	void push(const Value &v) { push(v, default); }
-    void push(const Value &v, const Priority &p);
-    void pop();
-
+	void push(const Value &v, const Priority &p);
+	
+	// Извлесение из очереди
+	void pop();
+	
+	// Просмотр первого элемента
 	Value top() const;
-    bool empty() const;
 
+	// Проверка очереди на пустоту
+	bool empty() const;
+
+	// Операция ввода элемента в очередь
 	priority_queue &operator<<(const Value &v);
+	// Операция вывода элемента из очереди
 	priority_queue &operator>>(Value &v);
-	priority_queue operator+(const priority_queue &q);
 
+	// Слияние двух очередей - новая очередь
+	priority_queue operator+(const priority_queue &q) const;
+
+	// Дружественная функция вывода
 	template <typename P, typename V>
 	friend std::ostream &operator<<(std::ostream &os, const priority_queue<P, V> &q);
-	template <typename P, typename V>
-	friend std::istream &operator>>(std::istream &is, priority_queue<P, V> &q);
 };
+
+template<typename P, typename V>
+std::istream & operator >> (std::istream & is, priority_queue<P, V>& q);
+
+template<typename P, typename V>
+inline std::ostream & operator<<(std::ostream & os, const priority_queue<P, V>& q)
+{
+	for (bst<qpair<P, V>>::iterator i = q.begin(); i != q.end(); ++i)
+		os << "{" << i->key << " " << i->value << "} ";
+	return os;
+}
