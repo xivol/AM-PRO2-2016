36639bac02a0173520ac4b528767ab09949aa20d
xivol <social.aurgtangfovr@gmail.com>
2016-12-21 21:35:05 +0300

    gender

--- Functors/gendered.cpp
+++ Functors/gendered.cpp
@@ -1,12 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 #include "gendered.h"
 
+using namespace std;
 
+int main()
+{   
+    setlocale(LC_ALL, "Russian");
+    ifstream fin("snames.txt");  
 
-gendered::gendered()
-{
-}
+    vector<string> names;
+    string fio;
+    while (getline(fin, fio, ';')) {
+        names.push_back(fio);
+    }
+    fin.close();
 
+    // Вычислить соотношение количества студентов женского и мужского пола
+    is_gender is_male(gender::Male);
+    is_gender is_female(gender::Female);
+    size_t males = count_if(names.begin(), names.end(), is_male);
+    size_t females = count_if(names.begin(), names.end(), is_female);
+    cout << females * 1.0 / males << endl;
 
-gendered::~gendered()
-{
+    // Вычислить количествол студентов с не русскими отчествами
+    is_gender is_foreign(gender::Unrecognised);
+    size_t foreign = count_if(names.begin(), names.end(), is_foreign);
+    cout << foreign << endl;
+
+    // Собрать не русские имена в новый вектор
+    vector<string> foreign_names(foreign);
+    copy_if(names.begin(), names.end(), foreign_names.begin(), is_foreign);
+        
+    for (string name : foreign_names)
+        cout << name << endl;
+
+    system("pause");
+    return 0;
 }
--- Functors/gendered.h
+++ Functors/gendered.h
@@ -1,14 +1,28 @@
 #pragma once
 #include <string>
-enum class gender{Male, Female, Unrecognised};
 
-class gendered
+// Пол
+enum class gender { Male, Female, Unrecognised };
+
+// Функтор - предикат сравнения с полом
+class is_gender
 {
+    // Искомый пол
 	gender target;
-	gender get_gender(std::string name);
+
+    // Вычисление пола по ФИО
+    gender get_gender(std::string name) {
+        switch (name[name.length() - 1]) {
+            case 'ч':  return gender::Male;
+            case 'а':  return gender::Female;
+            default :  return gender::Unrecognised;
+        }        
+    }
 public:
-	gendered(gender g): target(g) {}
-	bool operator(const std::string &name) {
+	is_gender(gender g): target(g) {}
+    
+    // Унарный предикат
+	bool operator()(const std::string &name) {
 		return get_gender(name) == target;
 	}
 };
